c69b465c7b62bbfc1d3beb4b36a44a2bf86821a2
Omid-Neshati <tchandnofame@gmail.com>
2022-04-20 03:19:21 +0330

    change cart indicator

--- src/components/Layout/Navbar/NavbarCart.tsx
+++ src/components/Layout/Navbar/NavbarCart.tsx
@@ -6,7 +6,7 @@ const NavbarCart = () => {
   return (
     <a className="relative flex justify-center px-3 py-2 text-4xl text-center bg-white rounded-md shadow-md shadow-gray-300 w-14">
       <BsCart3 />
-      <div className="rounded-full text-sm flex items-center justify-center w-5 h-5 -bottom-1 bg-red-600 shadow-sm shadow-red-600 text-white -right-1 border-[1px] absolute border-red-600 p-1">
+      <div className="absolute flex items-center justify-center p-2 text-sm text-white bg-red-600 border-4 border-white rounded-full shadow-sm shadow-gray-200 h-7 w-7 -bottom-1 -right-1">
         2
       </div>
     </a>
