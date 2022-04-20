5e1055def89b480a10d37b3c13fa4bd314cece59
Omid-Neshati <tchandnofame@gmail.com>
2022-04-20 07:46:12 +0330

    add Indicator componenet

--- src/components/Layout/Navbar/NavbarCart.tsx
+++ src/components/Layout/Navbar/NavbarCart.tsx
@@ -1,14 +1,13 @@
-import React from "react";
+import { FC } from "react";
 import { IconBase } from "react-icons";
 import { BsCart3 } from "react-icons/bs";
+import CartNumberIndicator from "./CartNumberIndicator";
 
 const NavbarCart = () => {
   return (
     <a className="relative flex justify-center px-3 py-2 text-4xl text-center bg-white rounded-md shadow-md shadow-gray-300 w-14">
       <BsCart3 />
-      <div className="absolute flex items-center justify-center p-2 text-sm text-white bg-red-600 border-4 border-white rounded-full shadow-sm shadow-gray-200 h-7 w-7 -bottom-1 -right-1">
-        2
-      </div>
+      <CartNumberIndicator />
     </a>
   );
 };
