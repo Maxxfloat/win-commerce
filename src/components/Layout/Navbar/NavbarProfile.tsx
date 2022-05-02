a9b95888c7c3bde55565da71a6f44670e116a03a
Omid-Neshati <tchandnofame@gmail.com>
2022-05-02 03:57:38 +0330

    make smaller on mobile screen

--- src/components/Layout/Navbar/NavbarProfile.tsx
+++ src/components/Layout/Navbar/NavbarProfile.tsx
@@ -2,7 +2,7 @@ import { BiUser } from "react-icons/bi";
 
 const NavbarProfile = () => {
   return (
-    <a className="flex justify-center px-3 py-2 text-4xl text-center bg-white rounded-md shadow-md shadow-gray-300 w-14">
+    <a className="flex items-center justify-center px-3 py-2 text-3xl text-center bg-white rounded-md shadow-md md:text-4xl shadow-gray-300 w-14">
       <BiUser />
     </a>
   );
