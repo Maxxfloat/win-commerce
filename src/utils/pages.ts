60873929ee3df90bdae14c4ace6bacf052137a63
Omid-Neshati <tchandnofame@gmail.com>
2022-05-14 17:51:48 +0330

    make pages hard code

---
+++ src/utils/pages.ts
@@ -0,0 +1,7 @@
+const pages: { title: string; address: string }[] = [
+  { title: "home", address: "/" },
+  { title: "categories", address: "/categories" },
+  { title: "about", address: "/about" },
+];
+
+export default pages;
