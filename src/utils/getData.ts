2c57a364e4884ee0ec395704b28930700acd1db2
Omid-Neshati <tchandnofame@gmail.com>
2022-05-14 17:50:28 +0330

    make get data functions

---
+++ src/utils/getData.ts
@@ -0,0 +1,85 @@
+import get from "modules/get";
+import qs from "qs";
+
+const populateAllParam = qs.stringify(
+  {
+    populate: "*",
+  },
+  { encodeValuesOnly: true }
+);
+
+export const getSlider = async () =>
+  get(`/slides?${populateAllParam}`).then((res) => res.data);
+
+export const getClocks = async () => {
+  const query = qs.stringify(
+    {
+      populate: "*",
+      filters: {
+        category: {
+          name: {
+            $eq: "clock",
+          },
+        },
+      },
+    },
+    { encodeValuesOnly: true }
+  );
+
+  return get(`/products?${query}`).then((res) => res.data);
+};
+
+export const getCategories = async () => {
+  const query = qs.stringify(
+    {
+      populate: "*",
+    },
+    { encodeValuesOnly: true }
+  );
+  return get(`/categories?${query}`).then((res) => res.data);
+};
+
+export const getCars = async () => {
+  const query = qs.stringify(
+    {
+      populate: "*",
+      filters: {
+        category: {
+          name: {
+            $eq: "car",
+          },
+        },
+      },
+      pagination: {
+        start: 0,
+        limit: 2,
+      },
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
+
+  return get(`/products?${query}`).then((res) => res.data);
+};
+
+export const getProducts = async (
+  query: string = qs.stringify({ populate: "*" }, { encodeValuesOnly: true })
+  // query?: string
+) => {
+  return get(`/products?${query}`).then((res) => res.data);
+};
+
+export const getProduct = async (productSlug?: string | string[]) => {
+  try {
+    const query = qs.stringify({
+      populate: "*",
+      filters: {
+        name: { $eq: productSlug },
+      },
+    });
+    return await get(`/products?${query}`).then((res) => res.data);
+  } catch {
+    throw Error("NO DATA");
+  }
+};
