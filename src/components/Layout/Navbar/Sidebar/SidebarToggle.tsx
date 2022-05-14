910717527618061967f885da983bb4e3c0b7b9cd
Omid-Neshati <tchandnofame@gmail.com>
2022-05-14 18:02:59 +0330

    finish

--- src/components/Layout/Navbar/Sidebar/SidebarToggle.tsx
+++ src/components/Layout/Navbar/Sidebar/SidebarToggle.tsx
@@ -1,9 +1,9 @@
-import React, { FC, useState } from "react";
-import { SidebarType } from "types/UseStateTypes";
+import { FC, Dispatch, SetStateAction } from "react";
 import { FiMenu } from "react-icons/fi";
-import { CgClose } from "react-icons/cg";
 
-const SidebarToggle: FC<SidebarType> = ({ sidebarOpen, setSidebarOpen }) => {
+const SidebarToggle: FC<{
+  setSidebarOpen: Dispatch<SetStateAction<boolean>>;
+}> = ({ setSidebarOpen }) => {
   return (
     <button
       onClick={function () {
@@ -11,7 +11,7 @@ const SidebarToggle: FC<SidebarType> = ({ sidebarOpen, setSidebarOpen }) => {
       }}
       className="z-30 flex items-center justify-center w-12 h-12 text-3xl text-center text-black rounded-lg "
     >
-      {sidebarOpen ? <CgClose className="text-4xl " /> : <FiMenu />}
+      <FiMenu />
     </button>
   );
 };
