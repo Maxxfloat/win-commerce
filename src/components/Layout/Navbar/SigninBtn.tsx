780f2111756b7f97238249caec89117cfcf43461
Omid-Neshati <tchandnofame@gmail.com>
2022-05-14 18:01:07 +0330

    finish

---
+++ src/components/Layout/Navbar/SigninBtn.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+import { BiLogIn } from "react-icons/bi";
+const BigSigninBtn = () => {
+  return (
+    <Link href={"/auth/login"} passHref>
+      <button className="flex items-center justify-center px-3 py-2 text-center bg-white rounded-md border-[1px] border-gray-500 whitespace-nowrap">
+        <BiLogIn />
+        <span className="ml-3 text-sm font-semibold text-gray-600 ">
+          {/* <span className="hidden mr-1 lg:inline">Signup |</span> */}
+          Login
+        </span>
+      </button>
+    </Link>
+  );
+};
+
+export default BigSigninBtn;
