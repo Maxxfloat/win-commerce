4bafbe252a0bcab0bdc8feede37e7fc201410cf4
Omid-Neshati <tchandnofame@gmail.com>
2022-05-15 18:45:47 +0330

    make text capitalize

--- src/components/Layout/Navbar/NavbarTab.tsx
+++ src/components/Layout/Navbar/NavbarTab.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 const NavbarTab: FC<{ title: string; link: string }> = ({ title, link }) => {
   return (
     <Link href={link}>
-      <a className="flex items-center justify-center p-2 text-xl font-bold hover:text-gray-600">
+      <a className="flex items-center justify-center p-2 text-xl font-bold capitalize hover:text-gray-600">
         {title}
       </a>
     </Link>
