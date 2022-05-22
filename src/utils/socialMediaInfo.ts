26326e6e6300ec90671a679a9703bb1d28179daa
Omid-Neshati <tchandnofame@gmail.com>
2022-05-22 16:38:20 +0330

    add linkedin url

--- src/utils/socialMediaInfo.ts
+++ src/utils/socialMediaInfo.ts
@@ -4,7 +4,11 @@ import { IconType } from "react-icons";
 
 const socialMediaInfo: { url: string; logo: IconType; className?: string }[] = [
   { url: "mailto:startomid2020@gmail.com", logo: HiOutlineMail },
-  { url: "jkjk", logo: BsLinkedin, className: "text-blue-900" },
+  {
+    url: "https://www.linkedin.com/in/omid-neshati/",
+    logo: BsLinkedin,
+    className: "text-blue-900",
+  },
   { url: "https://github.com/Maxxfloat/win-commerce", logo: BsGithub },
 ];
 
