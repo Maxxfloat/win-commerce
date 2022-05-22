b1eacf3a0f500acd53862fdb9f308dc460465667
Omid-Neshati <tchandnofame@gmail.com>
2022-05-22 18:44:29 +0330

    change nubmer to 0

--- src/components/Layout/Navbar/CartNumberIndicator.tsx
+++ src/components/Layout/Navbar/CartNumberIndicator.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 const CartNumberIndicator: FC = () => {
   return (
     <div className="absolute flex items-center justify-center p-2 text-sm text-white bg-red-600 border-4 border-white rounded-full shadow-sm shadow-gray-200 h-7 w-7 -bottom-1 -right-1">
-      2
+      0
     </div>
   );
 };
