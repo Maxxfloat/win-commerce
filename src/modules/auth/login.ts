d6329a6cb16c1f18442e589272c811f8457bcbcc
Omid-Neshati <tchandnofame@gmail.com>
2022-05-22 16:10:00 +0330

    add login function

--- src/modules/auth/login.ts
+++ src/modules/auth/login.ts
@@ -1,15 +1,6 @@
 import axios from "axios";
-import { setCookie } from "nookies";
 import instance from "modules/apiInstance";
-import useUser from "hooks/useUser";
-export const login = async (data: { identifier: string; password: string }) => {
-  try {
-    const res = await instance.post("/auth/local", data);
-    if (res.status === 200) {
-      setCookie(null, "accessToken", res.data.jwt);
-    }
-    return res;
-  } catch {
-    throw Error;
-  }
-};
+import { LoginForm } from "types/AuthFormType";
+
+export const login = async (data: LoginForm) =>
+  await axios.post("/api/auth/callback/login", data);
