b612eae116d1cb74bfb579198a379070cebbb06d
Omid-Neshati <tchandnofame@gmail.com>
2022-05-22 18:44:58 +0330

    remove className and add style

--- src/utils/socialMediaInfo.ts
+++ src/utils/socialMediaInfo.ts
@@ -1,15 +1,26 @@
 import { BsLinkedin, BsGithub } from "react-icons/bs";
 import { HiOutlineMail } from "react-icons/hi";
 import { IconType } from "react-icons";
+import { CSSProperties } from "react";
 
-const socialMediaInfo: { url: string; logo: IconType; className?: string }[] = [
-  { url: "mailto:startomid2020@gmail.com", logo: HiOutlineMail },
+const socialMediaInfo: {
+  url: string;
+  logo: IconType;
+  style?: CSSProperties;
+}[] = [
+  {
+    url: "mailto:startomid2020@gmail.com",
+    logo: HiOutlineMail,
+  },
   {
     url: "https://www.linkedin.com/in/omid-neshati/",
     logo: BsLinkedin,
-    className: "text-blue-900",
+    style: { color: "#0e76a8" },
   },
-  { url: "https://github.com/Maxxfloat/win-commerce", logo: BsGithub },
+  {
+    url: "https://github.com/Maxxfloat/win-commerce",
+    logo: BsGithub,
+  },
 ];
 
 export default socialMediaInfo;
