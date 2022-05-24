6229188bfc373b04ec79ac7fb7a1e2591574e9d6
Omid-Neshati <tchandnofame@gmail.com>
2022-05-24 19:09:44 +0330

    remove signin method

--- src/components/Layout/Navbar/SigninBtn.tsx
+++ src/components/Layout/Navbar/SigninBtn.tsx
@@ -4,18 +4,20 @@ import React from "react";
 import { BiLogIn } from "react-icons/bi";
 const BigSigninBtn = () => {
   return (
-    // <Link href={"/auth/login"} passHref>
-    <button
-      className="flex items-center justify-center px-3 py-2 text-center bg-white rounded-md border-[1px] border-gray-500 whitespace-nowrap"
-      onClick={() => signIn()}
-    >
-      <BiLogIn />
-      <span className="ml-3 text-sm font-semibold text-gray-600 ">
-        {/* <span className="hidden mr-1 lg:inline">Signup |</span> */}
-        Sign in
-      </span>
-    </button>
-    // </Link>
+    <Link href={"/auth/login"} passHref>
+      <button
+        className="flex items-center justify-center px-3 py-2 text-center bg-white rounded-md border-[1px] border-gray-500 whitespace-nowrap"
+        // onClick={() =>
+        //   signIn("credentials", { callbackUrl: "/", redirect: false })
+        // }
+      >
+        <BiLogIn />
+        <span className="ml-3 text-sm font-semibold text-gray-600 ">
+          {/* <span className="hidden mr-1 lg:inline">Signup |</span> */}
+          Sign in
+        </span>
+      </button>
+    </Link>
   );
 };
 
