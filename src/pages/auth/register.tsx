3a15b7cad3a539aebea98bbc670dfd0968bfdce3
Omid-Neshati <tchandnofame@gmail.com>
2022-05-25 19:02:24 +0330

    make register page

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { FC } from "react";
 
-const register = () => {
+const Register: FC = () => {
   return <div>register</div>;
 };
 
-export default register;
+export default Register;
