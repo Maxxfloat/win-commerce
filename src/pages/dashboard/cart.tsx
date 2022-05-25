740bb8e5dc6840e16f9b8d70bbd83dd5db199746
Omid-Neshati <tchandnofame@gmail.com>
2022-05-25 19:01:31 +0330

    make pages

---
+++ src/pages/dashboard/cart.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div className="flex items-center justify-center font-bold text-8xl">
+      NOT COMPLETE
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="flex items-center justify-center font-bold text-8xl">
+      NOT COMPLETE
+    </div>
+  );
+};
+
+export default Dashboard;
